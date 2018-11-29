44129c7cfd571d94b1d317195d72286a35daca69
Stephen Cholvat <Scholvat@uwaterloo.ca>
2018-11-28 21:04:53 -0800

    Added authorization to each api call

--- helpers/notificationToken.js
+++ helpers/notificationToken.js
@@ -29,7 +29,7 @@ module.exports = {
       if (token) {
         ApiClient.post(
           `/users/${user._id}/notificationToken`,
-          { 'Authorization': 'Bearer ' + authToken },
+          {},
           {notificationToken: token}
         )
         .then(response => {})
